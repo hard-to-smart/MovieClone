5a9fe8213259469d21be9d9bb4f7dab3f3f9ce6a
INNO <2403punjabidivya@gmail.com>
2024-09-25 17:25:38 +0530

    shubh aarambh

---
+++ src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <section className='w-full h-[34rem] flex flex-col justify-center items-center bg-slate-800'>
+        <h1 className='text-white text-4xl'>
+            Welcome.
+        </h1>
+        <h2 className='text-white'>Millions of movies, TV shows and people to discover. Explore now.</h2>
+        
+    </section>
+  )
+}
+
+export default Banner
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import Banner from '../components/Banner'
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Banner/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
